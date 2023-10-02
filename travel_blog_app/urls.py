e671ff6b8d822c6ba14b300da9d07c9555d44d3d
DiegoEstela <die.estela@gmail.com>
2023-10-02 11:18:24 +0200

    admin

--- travel_blog_app/urls.py
+++ travel_blog_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, home, new_post
+from .views import index, home, new_post, search_post, create_author, create_category
 urlpatterns = [
-    path('', home),
-    path('index', index, name='index'),
+    path('', index, name='index'),
     path('new_post/', new_post, name='new_post'),
-
+    path('search/', search_post, name='search_post'),
+    path('create_author/', create_author, name='create_author'),
+    path('create_category/', create_category, name='create_category'),
 ]
--- travel_blog_app/views.py
+++ travel_blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
+from django.db.models import Q
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm , AuthorForm , CategoryForm
 
 def home(request):
     return render(request, r'travel_blog_app/base.html')
@@ -18,3 +19,33 @@ def new_post(request):
     else:
         form = PostForm()
     return render(request, r'travel_blog_app/new_post.html', {'form': form})
+
+def search_post(request):
+    query = request.GET.get('q')
+    if query:
+        results = Post.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
+    else:
+        results = []
+    return render(request, r'travel_blog_app/search_post.html', {'results': results, 'query': query})
+
+def create_author(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index') 
+    else:
+        form = AuthorForm()
+    return render(request, r'travel_blog_app/create_author.html', {'form': form})
+
+def create_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # O la URL a la que quieras redirigir
+    else:
+        form = CategoryForm()
+    return render(request, r'travel_blog_app/create_category.html', {'form': form})
